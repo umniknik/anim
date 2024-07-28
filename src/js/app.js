a5f8f6809075c28470841762ea8cb4dbdc3c8047
umniknik <kaaaaaljan@mail.ru>
2024-07-28 11:06:45 +0300

    ДЗ к занятию "9. Анимации и CSS - готово

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,28 @@
 const button = document.querySelector('button');
 const tooltip = document.querySelector('.tooltip');
 
-button.addEventListener('click', () => {
-  // Если на в подсказке нет класса анимации и скрытия анимации, то... 
-  if (tooltip.classList.contains('tooltip_animation_reverse') || !tooltip.classList.contains('tooltip_animation')) { 
-
-    //Если в подсказке есть класс скрытия анимации, то ...
-    if (tooltip.classList.contains('tooltip_animation_reverse')) {
-      tooltip.classList.remove('tooltip_animation_reverse');   // то удаляем его 
-    }
-      
-    tooltip.classList.add('tooltip_animation'); // добавляем класс открытия анимации
-
-  } else {
-    tooltip.classList.remove('tooltip_animation');   // удаляем класс открытия анимации
-    tooltip.classList.add('tooltip_animation_reverse');      // добавляем класс скрытия анимации
-  }
-
+button.addEventListener( 'click' , () => {
+  tooltip.classList.toggle('tooltip_show');
+  button.classList.toggle('button-shadow');
 })
+
+// button.addEventListener('click', () => {
+//   // Если на в подсказке нет класса анимации и скрытия анимации, то... 
+//   if (tooltip.classList.contains('tooltip_animation_reverse') || !tooltip.classList.contains('tooltip_animation')) { 
+
+//     //Если в подсказке есть класс скрытия анимации, то ...
+//     if (tooltip.classList.contains('tooltip_animation_reverse')) {
+//       tooltip.classList.remove('tooltip_animation_reverse');   // то удаляем его 
+//     }
+      
+//     tooltip.classList.add('tooltip_animation'); // добавляем класс открытия анимации
+
+//   } else {
+//     tooltip.classList.remove('tooltip_animation');   // удаляем класс открытия анимации
+//     tooltip.classList.add('tooltip_animation_reverse');      // добавляем класс скрытия анимации
+//   }
+
+// })
 
 
 
